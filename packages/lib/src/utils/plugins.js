95627edb15b6d7239695a4e7fc40271c8cc69bb1
William Hilton <wmhilton@gmail.com>
2018-10-19 21:58:25 -0400

    fin

--- packages/lib/src/utils/plugins.js
+++ packages/lib/src/utils/plugins.js
@@ -1,7 +1,59 @@
 import { E, GitError } from '../models/GitError'
-import { PluginCore } from '../models/PluginCore.js'
 
-let _cores = new Map()
+class PluginCore extends Map {
+  set (key, value) {
+    const verifySchema = (key, value) => {
+      const pluginSchemas = {
+        fs: [
+          'lstat',
+          'mkdir',
+          'readdir',
+          'readFile',
+          'rmdir',
+          'stat',
+          'unlink',
+          'writeFile'
+        ],
+        credentialManager: ['fill', 'approved', 'rejected'],
+        emitter: ['emit']
+      }
+      if (!pluginSchemas.hasOwnProperty(key)) {
+        throw new GitError(E.PluginUnrecognized, { plugin: key })
+      }
+      for (let method of pluginSchemas[key]) {
+        if (value[method] === undefined) {
+          throw new GitError(E.PluginSchemaViolation, { plugin: key, method })
+        }
+      }
+    }
+    verifySchema(key, value)
+    if (key === 'fs') {
+      // There can be only one.
+      super.set(key, value)
+    }
+    if (key === 'credentialManager') {
+      // There can be only one.
+      super.set(key, value)
+    }
+    if (key === 'emitter') {
+      // There can be only one.
+      super.set(key, value)
+    }
+  }
+  get (key) {
+    // Critical plugins throw an error instead of returning undefined.
+    const critical = new Set(['fs', 'credentialManager'])
+    if (!super.has(key) && this.critical.has(key)) {
+      throw new GitError(E.PluginUndefined, { plugin: key })
+    }
+    return super.get(key)
+  }
+}
+
+// 99.99% of the time you can simply import { plugins } instead of cores.
+export const plugins = new PluginCore()
+
+let _cores = new Map(['default', plugins])
 
 export const cores = {
   // 'get' validates that a core has been registered
@@ -26,6 +78,3 @@ export const cores = {
     }
   }
 }
-
-// 99.99% of the time you can simply import { plugins } instead of cores.
-export const plugins = () => cores.get('default')
